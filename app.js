1c9d2846d3fdca454116755fdc25afbc722fe959
Chirag Thaker <chirag.thaker@bytestechnolab.in>
2022-09-30 11:02:33 +0530

    event loop examples

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const _ = require('lodash')
-const items = [1,[2,3],4,[5,6]];
+const http = require('http')
 
-const flattenArr = _.flattenDeep(items);
-console.log(flattenArr);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+      res.end('home page')
+  }
+  if (req.url === '/about') {
+    //   BLOCKING CODE
+      for (let i = 0; i < 1000; i++) {
+          for (let j = 0; j < 100; j++) {
+              console.log(i,j);
+          }
+      }
+      res.end('about page')
+  }
+  res.end('404 page not found')
+})
+
+server.listen(5001, () => {
+  console.log('Server listening on port : 5001....')
+})
